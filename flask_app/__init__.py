217a9703c2b60e0c744a6c9d33b9f7721c08e257
abishek1062 <abishek.s@outlook.com>
2020-10-12 00:29:14 +0530

    ddl Flask structure

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -17,7 +17,6 @@ app.logger.addHandler(logHandler)
 app.logger.setLevel(logging.INFO)
 
 
-# CORS(app)
 # Create a URL route in our application for "/"
 methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'COPY', 'HEAD', 'OPTIONS', 'LINK', 'UNLINK', 'PURGE', 'LOCK', 'UNLOCK', 'PROPFIND', 'VIEW']
 
@@ -33,10 +32,6 @@ def recognizeImage():
 
     model = get_model()
 
-    # breakpoint()
-
-    # get sample outputs
-    # breakpoint()
     output_tensor = model(image.cuda())
     output_tensor = Softmax(dim=1)(output_tensor)
     prob_pred_tensor, pred_tensor = torch.max(output_tensor, 1)
@@ -56,8 +51,3 @@ def recognizeImage():
 
 
     return jsonify( prediction = pred_dict, message = "success!" )
-
-# RUN FLASK APPLICATION
-if __name__ == '__main__':
-    # RUNNNING FLASK APP
-    app.run(debug=True, host = '0.0.0.0', port=5000)
